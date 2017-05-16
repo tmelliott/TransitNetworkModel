5fd701eb53395ddaea80453fa5e2920a6595b03b
Tom Elliott <tomelliottnz@gmail.com>
2017-05-17 09:12:53 +1200

    :wrench: Copy particle now working

--- include/gtfs.h
+++ include/gtfs.h
@@ -31,7 +31,7 @@ private:
 	std::vector<gtfs::Particle> particles_;
 	unsigned long next_id_;
 
-	friend class Particle;
+	friend class Particle; // Particle has access to vehicle.
 
 public:
 	// Constructors, destructors
@@ -61,22 +61,25 @@ public:
 class gtfs::Particle {
 private:
 	unsigned long particle_id_;
-	// gtfs::Vehicle& vehicle_;
+	gtfs::Vehicle* vehicle_;
+	unsigned long parent_id_;
 
 public:
 	// Constructors, destructors
-	Particle (gtfs::Vehicle& v);
+	Particle (gtfs::Vehicle* v);
 	Particle (const gtfs::Particle &p);
 	~Particle ();  // destroy
 
 	// Getters
 	const unsigned long particle_id () const;
+	gtfs::Vehicle* vehicle ();
+	const unsigned long parent_id () const;
 
 	// Setters
 
 
 	// Methods
-	//
+
 };
 
 
