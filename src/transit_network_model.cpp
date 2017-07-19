22676e2a74209b33c3de3fc9abf8981f165c6210
Tom Elliott <tomelliottnz@gmail.com>
2017-07-19 15:20:36 +1200

    Full network

--- src/transit_network_model.cpp
+++ src/transit_network_model.cpp
@@ -482,23 +482,23 @@ bool load_feed (std::unordered_map<std::string, std::unique_ptr<gtfs::Vehicle> >
 	// Cycle through feed entities and update associated vehicles, or create a new one.
 	std::vector<std::string> KEEPtrips;
 
-	sqlite3* db;
-	sqlite3_stmt* tripskeep;
-	std::string qry = "SELECT trip_id FROM trips WHERE route_id IN "
-		"(SELECT route_id FROM routes WHERE route_short_name IN "
-		"('274','277','224','222','258','NEX','129'))";
-		// "('274'))";
-	if (sqlite3_open (gtfs.get_dbname ().c_str (), &db)) {
-		std::cerr << "\n x oops...";
-	} else if (sqlite3_prepare_v2 (db, qry.c_str (), -1, &tripskeep, 0) != SQLITE_OK) {
-		std::cerr << "\n x oops2...";
-
-	} else {
-		while (sqlite3_step (tripskeep) == SQLITE_ROW) {
-			std::string t = (char*)sqlite3_column_text (tripskeep, 0);
-			KEEPtrips.push_back (t);
-		}
-	}
+	// sqlite3* db;
+	// sqlite3_stmt* tripskeep;
+	// std::string qry = "SELECT trip_id FROM trips WHERE route_id IN "
+	// 	"(SELECT route_id FROM routes WHERE route_short_name IN "
+	// 	"('274','277','224','222','258','NEX','129'))";
+	// 	// "('274'))";
+	// if (sqlite3_open (gtfs.get_dbname ().c_str (), &db)) {
+	// 	std::cerr << "\n x oops...";
+	// } else if (sqlite3_prepare_v2 (db, qry.c_str (), -1, &tripskeep, 0) != SQLITE_OK) {
+	// 	std::cerr << "\n x oops2...";
+	//
+	// } else {
+	// 	while (sqlite3_step (tripskeep) == SQLITE_ROW) {
+	// 		std::string t = (char*)sqlite3_column_text (tripskeep, 0);
+	// 		KEEPtrips.push_back (t);
+	// 	}
+	// }
 
 	for (int i=0; i<feed.entity_size (); i++) {
 		printf(" * Processing feed: %*d%%\r", 3, (int)(100 * (i+1) / feed.entity_size ()));
