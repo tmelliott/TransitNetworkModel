6e34f16157abd54defa825f45bc81b1ff6871425
Tom Elliott <tomelliottnz@gmail.com>
2017-09-06 14:45:02 +1200

    change routes used

--- src/transit_network_model.cpp
+++ src/transit_network_model.cpp
@@ -485,8 +485,8 @@ bool load_feed (std::unordered_map<std::string, std::unique_ptr<gtfs::Vehicle> >
 	sqlite3_stmt* tripskeep;
 	std::string qry = "SELECT trip_id FROM trips WHERE route_id IN "
 		"(SELECT route_id FROM routes WHERE route_short_name IN "
-		// "('274','277','224','222','258','NEX','129'))";
-		"('274'))";
+		"('274','277','224','222','258','NEX','129'))";
+		// "('274'))";
 	if (sqlite3_open (gtfs.get_dbname ().c_str (), &db)) {
 		std::cerr << "\n x oops...";
 	} else if (sqlite3_prepare_v2 (db, qry.c_str (), -1, &tripskeep, 0) != SQLITE_OK) {
