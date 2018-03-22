03b5168aefe21c1a128d76521b298a2632797bc4
Tom Elliott <tomelliottnz@gmail.com>
2018-03-23 08:39:59 +1300

    Make script runnable

--- R/model_segments.R
+++ R/model_segments.R
@@ -1,12 +1,15 @@
+cat(" * loading packages\n")
 suppressPackageStartupMessages({
     library(tidyverse)
     library(ggmap)
     library(viridis)
     library(RSQLite)
     library(Rcpp)
-    library(rgl)
+    ## library(rgl)
     library(sf)
     library(mgcv)
+    library(splines)
+    library(R2jags)
 })
 
 source("common.R")
@@ -19,17 +22,19 @@ as.time <- function(x) {
 }
 
 ## Load all of the data for exploring
-data <- dbGetQuery(dbConnect(SQLite(), "history.db"),
+cat(" * loading data\n")
+con <- dbConnect(SQLite(), "history.db")
+data <- dbGetQuery(con,
                    "SELECT * FROM vps WHERE segment_id IS NOT NULL") %>%
     as.tibble %>%
     mutate(speed = speed / 1000 * 60 * 60,
            time = as.time(timestamp),
            dow = format(as.POSIXct(timestamp, origin = "1970-01-01"), "%a"),
            weekend = dow %in% c("Sat", "Sun"))
+dbDisconnect(con)
 
-
-bbox <- c(174.7, -37, 174.9, -36.8)
-aklmap <- get_map(bbox, source = "stamen", maptype = "toner-lite")
+## bbox <- c(174.7, -37, 174.9, -36.8)
+## aklmap <- get_map(bbox, source = "stamen", maptype = "toner-lite")
 
 ##' @param data the data to plot
 ##' @param t time (in hours) to plot
@@ -63,38 +68,39 @@ doaplot <- function(data, segments, t, span = 0.25) {
     invisible(p)
 }
 
+cat(" * loading segments\n")
 segments <- getsegments()
 ## for (t in seq(5, 21, by = 0.25)) {
 ##     doaplot(data, segments, t)
 ## }
 
-for (si in unique(data$segment_id)) {
-    p <- ggplot(data %>% filter(segment_id == si), aes(time, speed)) +
-        geom_point() +
-        xlab("") + ylab("Speed (km/h)") + ylim(c(0, 100)) +
-        geom_smooth() + facet_wrap(~weekend, ncol = 1)
-    print(p)
-    grid::grid.locator()
-}
+## for (si in unique(data$segment_id)) {
+##     p <- ggplot(data %>% filter(segment_id == si), aes(time, speed)) +
+##         geom_point() +
+##         xlab("") + ylab("Speed (km/h)") + ylim(c(0, 100)) +
+##         geom_smooth() + facet_wrap(~weekend, ncol = 1)
+##     print(p)
+##     grid::grid.locator()
+## }
 
-#si <- "5262"
-si <- "5073"
-#si <- "2951"
+## si <- "5262"
+## si <- "5073"
+## si <- "2951"
 
-dsi <- data %>% filter(segment_id == si) %>%
-    mutate(route = substr(route_id, 1, 3))
-ggplot(dsi %>% filter(!weekend), aes(time, speed)) +
-    geom_point() +
-    xlab("") + ylab("Speed (km/h)") + ylim(c(0, 100)) +
-    geom_smooth()
+## dsi <- data %>% filter(segment_id == si) %>%
+##     mutate(route = substr(route_id, 1, 3))
+## ggplot(dsi %>% filter(!weekend), aes(time, speed)) +
+##     geom_point() +
+##     xlab("") + ylab("Speed (km/h)") + ylim(c(0, 100)) +
+##     geom_smooth()
 
-ssi <- segments %>% filter(id == si)
-bbox <- with(ssi, c(min(lng) - 0.05, min(lat) - 0.05,
-                    max(lng) + 0.05, max(lat) + 0.05))
-aklmap <- get_map(bbox, source = "stamen", maptype = "toner-lite")
+## ssi <- segments %>% filter(id == si)
+## bbox <- with(ssi, c(min(lng) - 0.05, min(lat) - 0.05,
+##                     max(lng) + 0.05, max(lat) + 0.05))
+## aklmap <- get_map(bbox, source = "stamen", maptype = "toner-lite")
 
-ggmap(aklmap) +
-    geom_path(aes(lng, lat), data = ssi, lwd = 2, col = "red")
+## ggmap(aklmap) +
+##     geom_path(aes(lng, lat), data = ssi, lwd = 2, col = "red")
 
 
 #col <- viridis(101)[round(dsi$speed)]
@@ -115,136 +121,133 @@ distIntoShape <- function(p, shape) {
 }
 
 
-dsi$dist <- distIntoShape(dsi, ssi)
+## dsi$dist <- distIntoShape(dsi, ssi)
 
-ggplot(dsi %>% filter(!weekend), aes(dist, speed, color = time)) +
-    geom_point() +
-    xlab("Distance (m)") + ylab("Speed (km/h)") + ylim(c(0, 100)) +
-    geom_smooth() +
-    scale_colour_viridis(option = "D")
+## ggplot(dsi %>% filter(!weekend), aes(dist, speed, color = time)) +
+##     geom_point() +
+##     xlab("Distance (m)") + ylab("Speed (km/h)") + ylim(c(0, 100)) +
+##     geom_smooth() +
+##     scale_colour_viridis(option = "D")
 
-fit <- gam(speed ~ s(dist) + s(time, k=10), data = dsi)
-summary(fit)
-xdist <- seq(min(dsi$dist), max(dsi$dist), length.out = 101)
-xtime <- seq(min(dsi$time), max(dsi$time), length.out = 101)
-pr <- expand.grid(xdist, xtime)
-names(pr) <- c("dist", "time")
-yspeed <- predict(fit, pr)
+## fit <- gam(speed ~ s(dist) + s(time, k=10), data = dsi)
+## summary(fit)
+## xdist <- seq(min(dsi$dist), max(dsi$dist), length.out = 101)
+## xtime <- seq(min(dsi$time), max(dsi$time), length.out = 101)
+## pr <- expand.grid(xdist, xtime)
+## names(pr) <- c("dist", "time")
+## yspeed <- predict(fit, pr)
 
-pred <- outer(xdist, xtime, function(d, t)
-    predict(fit, data.frame(dist = d, time = t)))
+## pred <- outer(xdist, xtime, function(d, t)
+##     predict(fit, data.frame(dist = d, time = t)))
 
-with(dsi %>% filter(!weekend),
-     plot3d(dist, time, speed, aspect = c(3, 5, 1)))
-surface3d(xdist, xtime, pred, grid=FALSE, color = "red")
-
-
-## A 'dummy' variable for "peak"
-peak <- c(9, 17)
-peak.sd <- c(0.5, 1)
-
-dsi <- dsi %>%
-    mutate(peak1 = dnorm(time, peak[1], peak.sd[1]),
-           peak2 = dnorm(time, peak[2], peak.sd[2]))
-
-p1 <- ggplot(dsi %>% filter(!weekend), aes(time, speed)) +
-    geom_point(aes(alpha = pmax(peak1 + peak2))) +
-    xlab("") + ylab("Speed (km/h)") + ylim(c(0, 100)) +
-    geom_smooth() +
-    geom_vline(xintercept = peak) +
-    theme(legend.position = "none")
-p2 <- ggplot() +
-    geom_path(aes(x = x, y = y),
-              data = data.frame(
-                  x = (xx <- seq(min(dsi$time), max(dsi$time), by = 0.01)),
-                  y = pmax(dnorm(xx, peak[1], peak.sd[1]),
-                           dnorm(xx, peak[2], peak.sd[2]))))
-gridExtra::grid.arrange(p1, p2, ncol = 1, heights = c(3, 1))
+## with(dsi %>% filter(!weekend),
+##      plot3d(dist, time, speed, aspect = c(3, 5, 1)))
+## surface3d(xdist, xtime, pred, grid=FALSE, color = "red")
 
 
-fit <- gam(speed ~ s(dist) + peak1 + peak2, data = dsi,
-           sp = c(-1, 1, 1))
-summary(fit)
-xdist <- seq(min(dsi$dist), max(dsi$dist), length.out = 101)
-xtime <- seq(min(dsi$time), max(dsi$time), length.out = 101)
+## ## A 'dummy' variable for "peak"
+## peak <- c(9, 17)
+## peak.sd <- c(0.5, 1)
 
-pred <- outer(xdist, xtime, function(d, t)
-    predict(fit, data.frame(dist = d,
-                            peak1 = dnorm(t, peak[1], peak.sd[1]),
-                            peak2 = dnorm(t, peak[2], peak.sd[2]))))
+## dsi <- dsi %>%
+##     mutate(peak1 = dnorm(time, peak[1], peak.sd[1]),
+##            peak2 = dnorm(time, peak[2], peak.sd[2]))
 
-with(dsi %>% filter(!weekend),
-     plot3d(dist, time, speed, aspect = c(3, 5, 1)))
-surface3d(xdist, xtime, pred, grid=FALSE, color = "red")
+## p1 <- ggplot(dsi %>% filter(!weekend), aes(time, speed)) +
+##     geom_point(aes(alpha = pmax(peak1 + peak2))) +
+##     xlab("") + ylab("Speed (km/h)") + ylim(c(0, 100)) +
+##     geom_smooth() +
+##     geom_vline(xintercept = peak) +
+##     theme(legend.position = "none")
+## p2 <- ggplot() +
+##     geom_path(aes(x = x, y = y),
+##               data = data.frame(
+##                   x = (xx <- seq(min(dsi$time), max(dsi$time), by = 0.01)),
+##                   y = pmax(dnorm(xx, peak[1], peak.sd[1]),
+##                            dnorm(xx, peak[2], peak.sd[2]))))
+## gridExtra::grid.arrange(p1, p2, ncol = 1, heights = c(3, 1))
 
-library(splines)
-library(R2jags)
+
+## fit <- gam(speed ~ s(dist) + peak1 + peak2, data = dsi,
+##            sp = c(-1, 1, 1))
+## summary(fit)
+## xdist <- seq(min(dsi$dist), max(dsi$dist), length.out = 101)
+## xtime <- seq(min(dsi$time), max(dsi$time), length.out = 101)
+
+## pred <- outer(xdist, xtime, function(d, t)
+##     predict(fit, data.frame(dist = d,
+##                             peak1 = dnorm(t, peak[1], peak.sd[1]),
+##                             peak2 = dnorm(t, peak[2], peak.sd[2]))))
+
+## with(dsi %>% filter(!weekend),
+##      plot3d(dist, time, speed, aspect = c(3, 5, 1)))
+## surface3d(xdist, xtime, pred, grid=FALSE, color = "red")
+
+
 
 ## gj <- jagam(speed ~ s(dist) + peak1 + peak2, data = dsi,
 ##             file = "model.jags")
 
-jags.data <- list(
-    B = (B <- bs(dsi$dist, df = 10)),
-    t = dsi$time,
-    y = dsi$speed,
-    N = nrow(dsi),
-    K = ncol(B)
-)
-jags.pars <- c("intercept", "beta", "tau", "omega", "pi", "alpha", "sig2", "r")
-##jags.pars <- c("intercept", "beta", "sig2")
+## jags.data <- list(
+##     B = (B <- bs(dsi$dist, df = 10)),
+##     t = dsi$time,
+##     y = dsi$speed,
+##     N = nrow(dsi),
+##     K = ncol(B)
+## )
+## jags.pars <- c("intercept", "beta", "tau", "omega", "pi", "alpha", "sig2", "r")
+## ##jags.pars <- c("intercept", "beta", "sig2")
 
-jfit <- jags(jags.data, NULL, jags.pars, model.file = "model.jags",
-             n.chains = 2, n.iter = 2000)
+## jfit <- jags(jags.data, NULL, jags.pars, model.file = "model.jags",
+##              n.chains = 2, n.iter = 2000)
 
-##jfit.mcmc <- as.mcmc.list(jfit$BUGSoutput)
+## ##jfit.mcmc <- as.mcmc.list(jfit$BUGSoutput)
 
-rdf <- jfit$BUGSoutput$sims.matrix %>% as.tibble %>%
-    rename_if(grepl("[", names(.), fixed=T),
-              function(x) gsub("`|\\[|\\]", "", x))
+## rdf <- jfit$BUGSoutput$sims.matrix %>% as.tibble %>%
+##     rename_if(grepl("[", names(.), fixed=T),
+##               function(x) gsub("`|\\[|\\]", "", x))
 
-hist(rdf$sig2, 50)
-hist(rdf$pi2, 50)
-hist(rdf$tau2, 50)
-hist(rdf$omega2, 50)
-hist(rdf$pi1, 50)
-hist(rdf$tau1, 50)
-hist(rdf$omega1, 50)
+## hist(rdf$sig2, 50)
+## hist(rdf$pi2, 50)
+## hist(rdf$tau2, 50)
+## hist(rdf$omega2, 50)
+## hist(rdf$pi1, 50)
+## hist(rdf$tau1, 50)
+## hist(rdf$omega1, 50)
 
-pairs(rdf %>% select_if(!grepl("beta|^r.", names(.))))
+## pairs(rdf %>% select_if(!grepl("beta|^r.", names(.))))
 
-sims <- jfit$BUGSoutput$median
-Bx <- bs(xdist, knots = attr(B, "knots"))
-nr <- nrow(sims[[1]])
-pred <- outer(1:length(xdist), xtime, function(j, t) {
-    p <- sapply(t, function(ti)
-        1 - sum(sims$r * sims$alpha * exp(-(ti - sims$tau)^2 / (2 * sims$omega^2))))
-    p * (sims$intercept[1] + Bx[j, ] %*% cbind(sims$beta))
-})
-with(dsi %>% filter(!weekend),
-     plot3d(dist, time, speed, aspect = c(3, 5, 1)))
-surface3d(xdist, xtime, pred, grid=FALSE, color = "#99000020")
-
-
-for (i in sample(nrow(rdf), 50)) {
-    sims <- lapply(jfit$BUGSoutput$sims.list, function(x) x[i, ])
-    pred <- outer(1:length(xdist), xtime, function(j, t) {
-        p <- sapply(t, function(ti)
-            1 - sum(sims$r * sims$alpha * exp(-(ti - sims$tau)^2 / (2 * sims$omega^2))))
-        p * (sims$intercept[1] + Bx[j, ] %*% cbind(sims$beta))
-    })
-    with(dsi %>% filter(!weekend),
-         plot3d(dist, time, speed, aspect = c(3, 5, 1)))
-    surface3d(xdist, xtime, pred, grid=FALSE, color = "#990000")
-    Sys.sleep(0.1)
-}
+## sims <- jfit$BUGSoutput$median
+## Bx <- bs(xdist, knots = attr(B, "knots"))
+## nr <- nrow(sims[[1]])
+## pred <- outer(1:length(xdist), xtime, function(j, t) {
+##     p <- sapply(t, function(ti)
+##         1 - sum(sims$r * sims$alpha * exp(-(ti - sims$tau)^2 / (2 * sims$omega^2))))
+##     p * (sims$intercept[1] + Bx[j, ] %*% cbind(sims$beta))
+## })
+## with(dsi %>% filter(!weekend),
+##      plot3d(dist, time, speed, aspect = c(3, 5, 1)))
+## surface3d(xdist, xtime, pred, grid=FALSE, color = "#99000020")
 
 
+## for (i in sample(nrow(rdf), 50)) {
+##     sims <- lapply(jfit$BUGSoutput$sims.list, function(x) x[i, ])
+##     pred <- outer(1:length(xdist), xtime, function(j, t) {
+##         p <- sapply(t, function(ti)
+##             1 - sum(sims$r * sims$alpha * exp(-(ti - sims$tau)^2 / (2 * sims$omega^2))))
+##         p * (sims$intercept[1] + Bx[j, ] %*% cbind(sims$beta))
+##     })
+##     with(dsi %>% filter(!weekend),
+##          plot3d(dist, time, speed, aspect = c(3, 5, 1)))
+##     surface3d(xdist, xtime, pred, grid=FALSE, color = "#990000")
+##     Sys.sleep(0.1)
+## }
 
-#si <- "5262"
-si <- "5073"
-#si <- "2951"
 
+cat(" * loading segments and computing distance\n")
 segs <- c("5262", "5073", "2951")
+st <- sort(table(data$segment_id), TRUE)
+segs <- names(st)[1:5]
 sg <- segments %>% filter(id %in% segs)
 ds <- data %>%
     filter(segment_id %in% segs) %>%
@@ -255,17 +258,17 @@ ds <- data %>%
                                       filter(id == segment_id[1])))) %>%
     ungroup()
 
-bbox <- with(sg, c(min(lng) - 0.05, min(lat) - 0.05,
-                   max(lng) + 0.05, max(lat) + 0.05))
-aklmap <- get_map(bbox, source = "stamen", maptype = "toner-lite")
+## bbox <- with(sg, c(min(lng) - 0.05, min(lat) - 0.05,
+##                    max(lng) + 0.05, max(lat) + 0.05))
+## aklmap <- get_map(bbox, source = "stamen", maptype = "toner-lite")
 
-ggmap(aklmap) +
-    geom_path(aes(lng, lat, colour = id), data = sg, lwd = 2)
+## ggmap(aklmap) +
+##     geom_path(aes(lng, lat, colour = id), data = sg, lwd = 2)
 
 
-ggplot(ds, aes(dist, speed, colour = time)) +
-    geom_point() +
-    facet_wrap(~segment_id, scales = "free", ncol = 1)
+## ggplot(ds, aes(dist, speed, colour = time)) +
+##     geom_point() +
+##     facet_wrap(~segment_id, scales = "free", ncol = 1)
 
 Bs <- tapply(ds$dist, ds$segment_id, bs, df = 10)
 jags.data <- list(
@@ -279,20 +282,28 @@ jags.data <- list(
 )
 jags.pars <- c("intercept", "beta", "tau", "omega", "pi", "alpha", "sig2", "r")
 
-jfit <- jags(jags.data, NULL, jags.pars, model.file = "model2.jags",
-             n.chains = 2, n.iter = 2000)
+cat(" * fitting JAGS model\n")
+jfit <- suppressMessages({
+    jags.parallel(jags.data, NULL, jags.pars,
+                  model.file = "model2.jags",
+                  n.chains = 4, n.iter = 5000)
+})
 
-rdf <- jfit$BUGSoutput$sims.matrix %>% as.tibble %>%
-    rename_if(grepl("[", names(.), fixed=T),
-              function(x)
-                  gsub(",", "_", gsub("`|\\[|\\]", "", x)))
+cat(" * writing results\n")
+save(ds, jfit, Bs, file = "model_results.rda")
 
-pairs(rdfx <- rdf %>% select_if(!grepl("beta|^r.", names(.))))
 
-op <- par(mfrow = c(5, 4))
-for (i in colnames(rdfx))
-    hist(rdf[[i]], 50, xlab=i,main=i)
-par(op)
+## rdf <- jfit$BUGSoutput$sims.matrix %>% as.tibble %>%
+##     rename_if(grepl("[", names(.), fixed=T),
+##               function(x)
+##                   gsub(",", "_", gsub("`|\\[|\\]", "", x)))
+
+## pairs(rdfx <- rdf %>% select_if(!grepl("beta|^r.", names(.))))
+
+## op <- par(mfrow = c(5, 4))
+## for (i in colnames(rdfx))
+##     hist(rdf[[i]], 50, xlab=i,main=i)
+## par(op)
 
 plotPlane <- function(x, fit, seg = 1, B,
                       which = c('median', 'mean', 'max', 'sample')) {
@@ -330,4 +341,7 @@ plotPlane <- function(x, fit, seg = 1, B,
     surface3d(xdist, xtime, pred, grid=FALSE, color = "#99000020")
 }
 
-plotPlane(ds, jfit, 2, Bs, which = 'median')
+## plotPlane(ds, jfit, 2, Bs, which = 'median')
+
+
+cat(" * done\n")
